eb520724550559d9ecad0e79255fd56cd58fa314
guibronzatti <guilherme.bronzatti@hotmail.com>
2020-11-08 20:57:15 -0300

    Update TestFoo.c

--- test/TestFoo.c
+++ test/TestFoo.c
@@ -256,6 +256,31 @@ void test_quickRandom(void)
   free(ptr);
 }
 
+void test_quickDescending(void)
+{
+  int tam = 4;
+  int array[10] = {10,9,8,7,6,5,4,3,2,1};
+  int num = 0;
+  char **ptr = (char **)malloc(tam * sizeof(char*));
+
+  ptr[0] = (char *)malloc(6 * sizeof(char));
+  sprintf(ptr[0], "quick");
+  ptr[1] = (char *)malloc(3 * sizeof(char));
+  sprintf(ptr[1], "10");
+  ptr[2] = (char *)malloc(11 * sizeof(char));
+  sprintf(ptr[2], "descending");
+  ptr[3] = (char *)malloc(3 * sizeof(char));
+  sprintf(ptr[3], "-P");
+    
+  TEST_ASSERT_EQUAL_INT(1,maincheck(tam,ptr,array,num));
+
+  free(ptr[0]);
+  free(ptr[1]);
+  free(ptr[2]);
+  free(ptr[3]);
+  free(ptr);
+}
+
 void test_quickAlmost(void)
 {
   int tam = 4;

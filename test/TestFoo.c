563665e21c8f96eea925f0a86d9b7fb0a4d45bef
guibronzatti <guilherme.bronzatti@hotmail.com>
2020-11-08 19:02:14 -0300

    Update TestFoo.c

--- test/TestFoo.c
+++ test/TestFoo.c
@@ -259,14 +259,14 @@ void test_quickRandom(void)
 void test_quickDescending(void)
 {
   int tam = 4;
-  int array[5] = {5,4,3,2,1};
+  int array[6] = {6,5,4,3,2,1};
   int num = 0;
   char **ptr = (char **)malloc(tam * sizeof(char*));
 
   ptr[0] = (char *)malloc(6 * sizeof(char));
   sprintf(ptr[0], "quick");
   ptr[1] = (char *)malloc(2 * sizeof(char));
-  sprintf(ptr[1], "5");
+  sprintf(ptr[1], "6");
   ptr[2] = (char *)malloc(11 * sizeof(char));
   sprintf(ptr[2], "descending");
   ptr[3] = (char *)malloc(3 * sizeof(char));

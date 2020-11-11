2df2ff9dfd3ee02bee0229864f2f23e1945bf071
guibronzatti <guilherme.bronzatti@hotmail.com>
2020-11-11 18:52:18 -0300

    Update TestFoo.c

--- TestFoo.c
+++ TestFoo.c
@@ -285,7 +285,7 @@ void test_quickAlmost(void)
 {
   int tam = 4;
   int array[5] = {0,1,2,4,3};
-  int num = 4;
+  int num = 3;
   char **ptr = (char **)malloc(tam * sizeof(char*));
 
   ptr[0] = (char *)malloc(6 * sizeof(char));
@@ -297,7 +297,7 @@ void test_quickAlmost(void)
   ptr[3] = (char *)malloc(3 * sizeof(char));
   sprintf(ptr[3], "-P");
     
-  TEST_ASSERT_EQUAL_INT(4,maincheck(tam,ptr,array,num));
+  TEST_ASSERT_EQUAL_INT(3,maincheck(tam,ptr,array,num));
 
   free(ptr[0]);
   free(ptr[1]);
